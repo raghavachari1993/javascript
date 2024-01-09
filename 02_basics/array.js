9e0ba2fb2a26559c53c598a2dce2341c2267ce91
raghavachari1993 <33456752+raghavachari1993@users.noreply.github.com>
2024-01-09 10:15:00 +0000

    container config done

---
+++ 02_basics/array.js
@@ -0,0 +1,14 @@
+const myArry = [9, 5, 6, 3, 7]
+const myFav = ["Shakthiman", "Hanuman", "Marvel"]
+const myArr2 = new Array(1, 2, 3, 4)
+//console.log(myArry[1]);
+//console.log(myFav[0]);
+//console.log(myArr2[3]);
+
+//Array Methods
+myArry.push(6, 11)
+//console.log(myArry);
+//console.log(myArry.includes(9));
+//console.log(myArry.indexOf(3));
+
+
